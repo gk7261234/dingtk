86b8362d231c6db2b766e475f911db37f0ef63b5
witt <wutai.f@vc265.com>
2018-06-19 15:34:50 +0800

    udpate: 列表渲染时校验用户身份

--- mobile/view/App.vue
+++ mobile/view/App.vue
@@ -1,66 +1,8 @@
 <template>
-  <div id="app" v-if="isUser">
+  <div id="app">
     <router-view></router-view>
   </div>
 </template>
-
-<script>
-  import crypto from 'crypto'
-  import {
-    cookie
-  } from 'vux'
-
-  export default {
-    name: 'app',
-    data() {
-      return {
-        isUser: false
-      }
-    },
-    mounted: async function () {
-      var _this = this
-      var _config
-
-      try {
-        let dAuth = await _this.$http.get('/apiQ/dAuth')
-        _config = dAuth.data
-      } catch (e) {
-        alert(e)
-      }
-
-      dd.config({
-        agentId: _config.agentId,
-        corpId: _config.corpId,
-        timeStamp: _config.timeStamp,
-        nonceStr: _config.nonceStr,
-        signature: _config.signature,
-        jsApiList: [
-          'biz.user.get'
-        ]
-      })
-
-      dd.error(function (e) {
-        alert('dd error: ' + JSON.stringify(e))
-      })
-
-      dd.ready(function () {
-        dd.biz.user.get({
-          corpId: _config.corpId,
-          onSuccess: function (data) {
-            if(data.nickName == '冯武泰') {
-              _this.isUser = true
-            } else {
-              alert('无权限访问此页面')
-            }
-          },
-          onFail: function (e) {
-            alert('user.get fail: ' + JSON.stringify(e))
-          }
-        })
-      })
-    }
-  }
-</script>
 
 <style lang="less">
   @import '~vux/src/styles/reset.less';
